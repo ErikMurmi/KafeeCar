06fdf85c3295fdf10ab956b2ccabf2502d23867b
erick <erick.paredes.nunez@udla.edu.ec>
2021-06-06 19:59:51 -0500

    Aumento de los xml catalogo_vendedor.xml // favoritos_cliente.xml // listacitas_cliente.xml // perfil_cliente.xml // y aumento de los fragment de catalogo_vendedor_fragment // citas_vendedor_fragment // favoritos_cliente_fragment // listacitas_cliente_fragment // perfil_cliente_fragment // ventas_vendedor_fragment con la implementacion de sus respectivos metodos Cuidado: se comento ciertos errir en los catalogos de administrador y vendedor

--- app/src/main/java/com/prog/kafeecar/Vendedores_Admin_Fragment.java
+++ app/src/main/java/com/prog/kafeecar/Vendedores_Admin_Fragment.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
