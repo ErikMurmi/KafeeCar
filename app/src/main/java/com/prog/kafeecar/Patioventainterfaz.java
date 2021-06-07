ceb8d58a7118095d23fc82fa6a3613888a2b233f
Dianitss <diana.cuenca.zambrano@udla.edu.ec>
2021-06-07 17:00:50 -0500

    Se eliminaron los toast y se activo el estado del vendedor

--- app/src/main/java/com/prog/kafeecar/Patioventainterfaz.java
+++ app/src/main/java/com/prog/kafeecar/Patioventainterfaz.java
@@ -523,7 +523,6 @@ public class Patioventainterfaz extends AppCompatActivity {
 
 
     public void registrarAdministrador() throws ParseException {
-        Toast.makeText(Patioventainterfaz.this, "Se inicio el metodo",Toast.LENGTH_SHORT).show();
         EditText nombreAdmin = findViewById(R.id.reg_nombre_admin_etxt);
         EditText apellidoAdmin = findViewById(R.id.reg_apellido_admin_etxt);
         EditText cedulaAdmin = findViewById(R.id.reg_cedula_admin_etxt);
@@ -537,7 +536,6 @@ public class Patioventainterfaz extends AppCompatActivity {
         EditText horaEntradaAdmin = findViewById(R.id.reg_entrada_admin_etxt);
         EditText horaSalidaAdmin = findViewById(R.id.reg_salida_admin_etxt);
         EditText horaAlmuerzoAdmin = findViewById(R.id.reg_almuerzo_admin_etxt);
-        Toast.makeText(Patioventainterfaz.this, "id's hechos",Toast.LENGTH_SHORT).show();
 
         String nombreAdmin_str = nombreAdmin.getText().toString() + "" + apellidoAdmin.getText().toString();
         String cedulaAdmin_str = cedulaAdmin.getText().toString();
@@ -568,9 +566,9 @@ public class Patioventainterfaz extends AppCompatActivity {
                     contraseniaVerificada,
                     sdf.parse(fechaNacimientoAdmin_date)), "Vendedor");
             irAplicacion("ADMIN");
-            Toast.makeText(Patioventainterfaz.this, "Se agrego al admin",Toast.LENGTH_SHORT).show();
+            Toast.makeText(Patioventainterfaz.this, "Administrador agregado correctamente",Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(Patioventainterfaz.this, "No se agrego al admin",Toast.LENGTH_SHORT).show();
+            Toast.makeText(Patioventainterfaz.this, "No se agregó el administrador",Toast.LENGTH_SHORT).show();
             contraseniaAdmin.setText("");
             confirmarContraseniaAdmin.setText("");
         }
