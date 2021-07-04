612a2ffaf2604d7a7a84acb75942424e701850fe
dianacuencazambrano <diana.cuenca.zambrano@udla.edu.ec>
2021-07-03 19:58:35 -0500

    se cambio el color del campo de texto del editable del perfil del admin y se añadio la clase y el codigo para visualizar la lista de vehiculos para el vendedor

--- app/src/main/java/com/prog/kafeecar/Catalogo_Vendedor_Fragment.java
+++ app/src/main/java/com/prog/kafeecar/Catalogo_Vendedor_Fragment.java
@@ -1,60 +1,41 @@
 package com.prog.kafeecar;
-import android.app.Activity;
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-//import com.bumptech.glide.Glide;
 
 import java.io.File;
 import java.io.IOException;
 
-import static java.lang.String.format;
-public class Catalogo_Vendedor_Fragment extends Fragment {
+public class Catalogo_Vendedor_Fragment extends Fragment implements Adaptador_Lista_Catalogo_vn.RecyclerItemClick {
 
-    private static final int REQUEST_IMAGE_GALERY = 101;
-    private String TAG = "Catalogo";
     private View mainView;
-
-    private Button agendarCita_btn;
-
-    private ImageButton buscarPlacaVn_btn;
 
     private ScrollView verVehiculo;
 
-    //lyts de los vendedores en la lista
     private LinearLayout verCatalogo;
-    private LinearLayout verVehiculoLista_lyt;
-    private LinearLayout verVehiculoLista1_lyt;
-
 
     private PatioVenta patio;
-
-    private Uri foto;
+    private Vehiculo vMostrar;
 
     private final StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
 
@@ -66,48 +47,12 @@ public class Catalogo_Vendedor_Fragment extends Fragment {
         mainView = inflater.inflate(R.layout.catalogo_vendedor, container, false);
         patio = Patioventainterfaz.patioventa;
 
-        try {
-            verLista("PSD-1234","GHC-2434");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         //Botones
-        buscarPlacaVn_btn = mainView.findViewById(R.id.busqueda_placa_vn_btn);
-        agendarCita_btn = mainView.findViewById(R.id.irAgendarCita_ca_vn_btn);
+        Button agendarCita_btn = mainView.findViewById(R.id.irAgendarCita_ca_vn_btn);
 
         //Layouts
         verCatalogo = mainView.findViewById(R.id.vehiculos_catalogo_vendedor);
         verVehiculo = mainView.findViewById(R.id.visualizar_vehiculo_ca_vn_sv);
-        verVehiculoLista_lyt = mainView.findViewById(R.id.vehiculo_lista_vendedor_lyt);
-        verVehiculoLista1_lyt = mainView.findViewById(R.id.vehiculo_lista1_vendedor_lyt);
-
-
-        verVehiculoLista_lyt.setOnClickListener(v -> {
-            //Desactivar otros diseños
-            verCatalogo.setVisibility(View.GONE);
-            //Activar el diseño deseadow
-            verVehiculo.setVisibility(View.VISIBLE);
-            try {
-                visualizarVehiculoVendedor("PSD-1234");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        verVehiculoLista1_lyt.setOnClickListener(v -> {
-            //Desactivar otros diseños
-            verCatalogo.setVisibility(View.GONE);
-            //Activar el diseño deseadow
-            verVehiculo.setVisibility(View.VISIBLE);
-            try {
-                visualizarVehiculoVendedor("GHC-2434");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-
 
         agendarCita_btn.setOnClickListener(v -> {
             verCatalogo.setVisibility(View.GONE);
@@ -115,77 +60,20 @@ public class Catalogo_Vendedor_Fragment extends Fragment {
             //aniadir la visibilidad del agendar una cita
         });
 
-        buscarPlacaVn_btn.setOnClickListener(view -> {
-            buscarVehiculos();
-        });
-
         return mainView;
     }
 
-
-    public void verLista(String placa, String placa1) throws Exception {
-
-        ImageView v_img = mainView.findViewById(R.id.v_lista_vendedor_img);
-        TextView titulo = mainView.findViewById(R.id.v_marca_modelo_vendedor_txt);
-        TextView anio = mainView.findViewById(R.id.v_anio_lista_vendedor_txt);
-        TextView matricula =  mainView.findViewById(R.id.v_matricula_lista_vendedor_txt);
-        TextView precio = mainView.findViewById(R.id.v_precio_lista_vendedor_txt);
-        TextView placa_v = mainView.findViewById(R.id.v_placa_lista_vendedor_txt);
-
-        ImageView v_img1 = mainView.findViewById(R.id.v_lista1_vendedor_img);
-        TextView titulo1 = mainView.findViewById(R.id.v_marca_modelo1_vendedor_txt);
-        TextView  anio1 = mainView.findViewById(R.id.v_anio_lista1_vendedor_txt);
-        TextView matricula1 =  mainView.findViewById(R.id.v_matricula_lista1_vendedor_txt);
-        TextView precio1 = mainView.findViewById(R.id.v_precio_lista1_vendedor_txt);
-        TextView placa_v1 = mainView.findViewById(R.id.v_placa_lista1_vendedor_txt);
-
-
-        Vehiculo v_Mostrar = patio.buscarVehiculos("Placa",placa);
-        titulo.setText(new String(v_Mostrar.getMarca()+" "+ v_Mostrar.getModelo()));
-        anio.setText(String.valueOf(v_Mostrar.getAnio()));
-        matricula.setText(v_Mostrar.getMatricula());
-        placa_v.setText(placa);
-        precio.setText(String.format("$ %.2f",v_Mostrar.getPrecioVenta()));
-
-
-        Vehiculo v_Mostrar1 = patio.buscarVehiculos("Placa",placa1);
-        titulo1.setText(new String(v_Mostrar1.getMarca()+" "+ v_Mostrar1.getModelo()));
-        placa_v1.setText(v_Mostrar1.getPlaca());
-        precio1.setText(String.format("$ %.2f",v_Mostrar1.getPrecioVenta()));
-        anio1.setText(String.valueOf(v_Mostrar1.getAnio()));
-        matricula1.setText(v_Mostrar1.getMatricula());
-
-        StorageReference filePath = mStorageRef.child("Vehiculos/"+v_Mostrar.getimagen());
-        Glide.with(mainView)
-                .load(filePath)
-                .into(v_img);
-        try {
-            final File localFile = File.createTempFile(v_Mostrar.getimagen(),"jpg");
-            filePath.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
-                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                v_img.setImageBitmap(bitmap);
-            });
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
-        //Imagen 2
-        filePath = mStorageRef.child("Vehiculos/"+v_Mostrar1.getimagen());
-        Glide.with(mainView)
-                .load(filePath)
-                .into(v_img1);
-        try {
-            final File localFile = File.createTempFile(v_Mostrar1.getimagen(),"jpg");
-            filePath.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
-                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                v_img1.setImageBitmap(bitmap);
-            });
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+    public void cargar() {
+        RecyclerView listaview = mainView.findViewById(R.id.lista_autos_vendedor_rv);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(mainView.getContext());
+        listaview.setLayoutManager(manager);
+        listaview.setItemAnimator(new DefaultItemAnimator());
+        Adaptador_Lista_Catalogo_vn adptadorlistaview = new Adaptador_Lista_Catalogo_vn(patio.getVehiculos(), this);
+        listaview.setAdapter(adptadorlistaview);
     }
 
-    public void visualizarVehiculoVendedor(String placa_buscar) throws Exception{
+    @SuppressLint("SetTextI18n")
+    public void visualizarVehiculoVendedor(){
         ImageView v_img = mainView.findViewById(R.id.vehiculo_ca_vn_img);
         TextView titulo = mainView.findViewById(R.id.auto_titulo_ca_vn_txt);
         TextView placa = mainView.findViewById(R.id.ver_placa_ca_vn_txt);
@@ -199,12 +87,6 @@ public class Catalogo_Vendedor_Fragment extends Fragment {
         TextView preciVenta = mainView.findViewById(R.id.ver_pventa_ca_vn_txt);
         TextView promocion = mainView.findViewById(R.id.ver_promocion_ca_vn_txt);
         TextView descripcion = mainView.findViewById(R.id.ver_descipcion_ca_vn_txt);
-        Vehiculo vMostrar  = null;
-        try {
-            vMostrar = patio.buscarVehiculos("Placa",placa_buscar);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         String titulo_str = vMostrar.getMarca()+" "+vMostrar.getModelo();
         titulo.setText(titulo_str);
@@ -240,37 +122,19 @@ public class Catalogo_Vendedor_Fragment extends Fragment {
         }
     }
 
-    public void buscarVehiculos () {
-        EditText placa = mainView.findViewById(R.id.buscar_placa_vendedor_etxt);
-        String placa_str = placa.getText().toString();
-        Vehiculo buscado = null;
-        try {
-            buscado = patio.buscarVehiculos("Placa",placa_str);
-            if (buscado == null) {
-                Toast.makeText(mainView.getContext(), "No existe el vehículo buscado", Toast.LENGTH_SHORT).show();
-            } else {
-                verLista(placa_str, "GHC-2434");
-            }
-        } catch (Exception e) {
-            Toast.makeText(mainView.getContext(), "No existe el vehiculo", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void openGalery(){
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, REQUEST_IMAGE_GALERY);
-    }
-
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == REQUEST_IMAGE_GALERY){
-            if(resultCode == Activity.RESULT_OK && data != null){
-                foto = data.getData();
-            }else{
-                Toast.makeText(mainView.getContext(), "No se ha insertado la imagen", Toast.LENGTH_SHORT).show();
-            }
+    public void itemClick(String placa) {
+        try {
+            vMostrar = patio.buscarVehiculos("Placa", placa);
+            irVer();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
+    private void irVer() {
+        verCatalogo.setVisibility(View.GONE);
+        verVehiculo.setVisibility(View.VISIBLE);
+        visualizarVehiculoVendedor();
+    }
 }
