e70ee11612dbd8de106f6f189ff1ac4262f004fc
Dianitss <diana.cuenca.zambrano@udla.edu.ec>
2021-07-17 17:54:46 -0500

    Perfil_Vendedor_Fragment revisado y se incluyó validaciones y mensajes de error

--- app/src/main/java/com/prog/kafeecar/Perfil_Vendedor_Fragment.java
+++ app/src/main/java/com/prog/kafeecar/Perfil_Vendedor_Fragment.java
@@ -23,38 +23,25 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOException;
 
-import javax.microedition.khronos.egl.EGLDisplay;
-
 public class Perfil_Vendedor_Fragment extends Fragment {
 
-
     private static final int REQUEST_IMAGE_GALERY = 101;
-    private String TAG = "Perfil vendedor";
     private View mainview;
     private Uri foto;
-    private Vendedor user = (Vendedor) Patioventainterfaz.usuarioActual;
+    private final Vendedor user = (Vendedor) Patioventainterfaz.usuarioActual;
     private PatioVenta patio;
     //Layouts
     private ScrollView perfil_lyt;
     private LinearLayout editar_perfil_lyt;
     private LinearLayout perfil_btns_lyt;
-
     //Imagenes
     private ImageView perfil_img;
-    //Botones
-    private Button cancelar;
-    private Button irEditar;
-    private  Button guardarEditar;
-
     //Botones con imagen
     private ImageButton ven_img_btn;
 
@@ -73,9 +60,10 @@ public class Perfil_Vendedor_Fragment extends Fragment {
         perfil_img = mainview.findViewById(R.id.ven_img);
         //Botones
         ven_img_btn = mainview.findViewById(R.id.vendedor_pe_vn_btn);
-        cancelar = mainview.findViewById(R.id.cancelar_edit_pe_vn_lyt);
-        irEditar = mainview.findViewById(R.id.ireditar_ven_btn);
-        guardarEditar = mainview.findViewById(R.id.editar_pe_vn_lyt);
+        //Botones
+        Button cancelar = mainview.findViewById(R.id.cancelar_edit_pe_vn_lyt);
+        Button irEditar = mainview.findViewById(R.id.ireditar_ven_btn);
+        Button guardarEditar = mainview.findViewById(R.id.editar_pe_vn_lyt);
         verPerfil();
 
         irEditar.setOnClickListener(v -> {
@@ -92,12 +80,9 @@ public class Perfil_Vendedor_Fragment extends Fragment {
             msg.setPositiveButton("Aceptar", (dialog, which) -> editarVendedor());
             msg.setNegativeButton("Cancelar", (dialog, which) -> dialog.cancel());
             msg.show();
-
         });
 
-        ven_img_btn.setOnClickListener(v -> {
-            openGalery();
-        });
+        ven_img_btn.setOnClickListener(v -> openGalery());
 
         cancelar.setOnClickListener(v -> {
             irPerfil();
@@ -125,25 +110,20 @@ public class Perfil_Vendedor_Fragment extends Fragment {
             cedula_ed.setText(user.getCedula());
             telefono_ed.setText(user.getTelefono());
             correo_ed.setText(user.getCorreo());
+
             StorageReference filePath = mStorageRef.child("Vendedores/"+user.getImagen());
-            Glide.with(mainview)
-                    .load(filePath)
-                    .into(perfil_img);
+
             try {
                 final File localFile = File.createTempFile(user.getImagen(),"jpg");
-                filePath.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                        Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                        perfil_img.setImageBitmap(bitmap);
-                    }
+                filePath.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    perfil_img.setImageBitmap(bitmap);
                 });
             }catch (IOException e) {
-                Toast.makeText(mainview.getContext(), "catch", Toast.LENGTH_SHORT).show();
-                e.printStackTrace();
+                Toast.makeText(mainview.getContext(), "Error 176: No se pudo cargar la imagen ", Toast.LENGTH_SHORT).show();
             }
         }catch (Exception e){
-            Toast.makeText(mainview.getContext(), "No se puede mostrar la información", Toast.LENGTH_SHORT).show();
+            Toast.makeText(mainview.getContext(), "Error 177: No se puede mostrar la información", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -171,175 +151,173 @@ public class Perfil_Vendedor_Fragment extends Fragment {
             cedula_ed.setText(user.getCedula());
             telefono_ed.setText(user.getTelefono());
             correo_ed.setText(user.getCorreo());
+            contrasenia_ed.setText("");
+            confirm_c_ed.setText("");
 
             StorageReference filePath = mStorageRef.child("Vendedores/"+user.getImagen());
-            Glide.with(mainview)
-                    .load(filePath)
-                    .into(ven_img_btn);
             try {
                 final File localFile = File.createTempFile(user.getImagen(),"jpg");
-                filePath.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                        Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                        ven_img_btn.setImageBitmap(bitmap);
-                    }
+                filePath.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    ven_img_btn.setImageBitmap(bitmap);
                 });
             }catch (IOException e) {
-                e.printStackTrace();
+                Toast.makeText(mainview.getContext(), "Error 178: No se pudo cargar la imagen ", Toast.LENGTH_SHORT).show();
             }
 
         }catch (Exception e){
-            Toast.makeText(mainview.getContext(), "No se puede mostrar la información", Toast.LENGTH_SHORT).show();
+            Toast.makeText(mainview.getContext(), "Error 179: No se puede mostrar la información", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void editarVendedor(){
-            boolean cambiar_clave = false;
-            int c = 0;
-            EditText nombre_ed = mainview.findViewById(R.id.nombre_pe_vn_etxt);
-            String nombre_str = nombre_ed.getText().toString();
-            if(nombre_str.isEmpty()){
-                Toast.makeText(mainview.getContext(), "Campo vacío: *Nombre*", Toast.LENGTH_SHORT).show();
-                c++;
-            }
+        boolean cambiar_clave = false;
+        int vacios = 0;
+        int invalidos = 0;
 
-            EditText anio_ed = mainview.findViewById(R.id.anioNacimiento_pe_vn_etxt);
-            String anio_str = anio_ed.getText().toString();
-            int anio = -1;
-            if(anio_str.isEmpty()){
-                Toast.makeText(mainview.getContext(), "Campo vacío: *Año*", Toast.LENGTH_SHORT).show();
-                c++;
+        EditText nombre_ed = mainview.findViewById(R.id.nombre_pe_vn_etxt);
+        String nombre_str = nombre_ed.getText().toString();
+        if(nombre_str.isEmpty()){
+            vacios++;
+        }
+
+        EditText anio_ed = mainview.findViewById(R.id.anioNacimiento_pe_vn_etxt);
+        String anio_str = anio_ed.getText().toString();
+        int anio = -1;
+        if(anio_str.isEmpty()){
+            vacios++;
+        }else{
+            anio = Integer.parseInt(anio_ed.getText().toString());
+            if (anio < 1900 || anio > 2003) {
+                Toast.makeText(mainview.getContext(), "Año inválido", Toast.LENGTH_SHORT).show();
+                anio_ed.setText("");
+                invalidos++;
+            }
+        }
+        EditText mes_ed = mainview.findViewById(R.id.mesNacimiento_pe_vn_etxt);
+        String mes_str = mes_ed.getText().toString();
+        int mes = -1;
+        if(mes_str.isEmpty()){
+            vacios++;
+        }else{
+            mes = Integer.parseInt(mes_str);
+            if (mes < 1 || mes > 12) {
+                Toast.makeText(mainview.getContext(), "Mes inválido", Toast.LENGTH_SHORT).show();
+                mes_ed.setText("");
+                invalidos++;
+            }
+        }
+
+        EditText dia_ed = mainview.findViewById(R.id.diaNacimiento_pe_vn_etxt);
+        String dia_str = dia_ed.getText().toString();
+        int dia;
+        if(dia_str.isEmpty()){
+            vacios++;
+        }else{
+            dia = Integer.parseInt(dia_str);
+            if (!Patioventainterfaz.validarDia(anio, mes, dia)) {
+                Toast.makeText(mainview.getContext(), "Día inválido", Toast.LENGTH_SHORT).show();
+                dia_ed.setText("");
+                invalidos++;
+            }
+        }
+
+        EditText cedula_ed = mainview.findViewById(R.id.cedula_pe_vn_etxt);
+        String cedula_str = cedula_ed.getText().toString();
+        if(cedula_str.isEmpty()){
+            vacios++;
+        }else{
+            if(cedula_str.length()!=10){
+                Toast.makeText(mainview.getContext(), "Número de cédula inválido", Toast.LENGTH_SHORT).show();
+                cedula_ed.setText("");
+                invalidos++;
+            }
+        }
+
+        EditText telefono_ed = mainview.findViewById(R.id.telefono_pe_vn_etxt);
+        String telefono_str = telefono_ed.getText().toString();
+        if(telefono_str.isEmpty()){
+            vacios++;
+        }else{
+            if (telefono_str.length() != 10) {
+                Toast.makeText(mainview.getContext(), "Numero de telefono invalido", Toast.LENGTH_SHORT).show();
+                telefono_ed.setText("");
+                invalidos++;
+            }
+        }
+
+        EditText correo_ed = mainview.findViewById(R.id.correo_pe_vn_etxt);
+        String correo_str = correo_ed.getText().toString();
+        if(correo_str.isEmpty()){
+            vacios++;
+        }else{
+            if (!Patioventainterfaz.validarMail(correo_str)) {
+                Toast.makeText(mainview.getContext(), "Correo no valido", Toast.LENGTH_SHORT).show();
+                correo_ed.setText("");
+                vacios++;
+            }
+        }
+
+        EditText textoclave = mainview.findViewById(R.id.contrasenia_pe_vn_etxt);
+        String clave_str = textoclave.getText().toString();
+        EditText textorepetirclave = mainview.findViewById(R.id.clave_confirm_pe_vn_etxt);
+        String repetirclave_str = textorepetirclave.getText().toString();
+        if(!clave_str.isEmpty() && !repetirclave_str.isEmpty()){
+            if(clave_str.length()<8 || repetirclave_str.length()<8){
+                Toast.makeText(mainview.getContext(), "La clave debe contener 8 caracteres mínimo", Toast.LENGTH_SHORT).show();
+                textoclave.setText("");
+                textorepetirclave.setText("");
+                invalidos++;
             }else{
-                anio = Integer.parseInt(anio_ed.getText().toString());
-                if (anio < 1900 || anio > 2003) {
-                    Toast.makeText(mainview.getContext(), "Año inválido", Toast.LENGTH_SHORT).show();
-                    anio_ed.setText("");
-                    c++;
+                if ( (clave_str.compareTo(repetirclave_str) != 0)) {
+                    Toast.makeText(mainview.getContext(), "Las claves no coinciden", Toast.LENGTH_SHORT).show();
+                    textoclave.setText("");
+                    textorepetirclave.setText("");
+                    invalidos++;
+                }else{
+                    cambiar_clave = true;
                 }
             }
-            EditText mes_ed = mainview.findViewById(R.id.mesNacimiento_pe_vn_etxt);
-            String mes_str = mes_ed.getText().toString();
-            int mes = -1;
-            if(mes_str.isEmpty()){
-                Toast.makeText(mainview.getContext(), "Campo vacío: *Mes*", Toast.LENGTH_SHORT).show();
-                c++;
-            }else{
-                mes = Integer.parseInt(mes_str);
-                if (mes < 1 || mes > 12) {
-                    Toast.makeText(mainview.getContext(), "Mes inválido", Toast.LENGTH_SHORT).show();
-                    mes_ed.setText("");
-                    c++;
+        }else{
+            vacios++;
+        }
+
+        if (vacios == 0 && invalidos == 0) {
+            String fecha = dia_str + "-" + mes_str + "-" + anio_str;
+            try {
+                if(cambiar_clave) {
+                    user.cambiarDatosVendedor(
+                        nombre_ed.getText().toString(),
+                        cedula_ed.getText().toString(),
+                        telefono_ed.getText().toString(),
+                        correo_ed.getText().toString(),
+                        fecha,
+                        clave_str);
+                }else{
+                    user.cambiarDatosVendedor(
+                        nombre_ed.getText().toString(),
+                        cedula_ed.getText().toString(),
+                        telefono_ed.getText().toString(),
+                        correo_ed.getText().toString(),
+                        fecha,
+                        user.getClave());
                 }
-            }
-
-            EditText dia_ed = mainview.findViewById(R.id.diaNacimiento_pe_vn_etxt);
-            String dia_str = dia_ed.getText().toString();
-            int dia = -1;
-            if(dia_str.isEmpty()){
-                Toast.makeText(mainview.getContext(), "Campo vacío: *Día*", Toast.LENGTH_SHORT).show();
-                c++;
-            }else{
-                dia = Integer.parseInt(dia_str);
-                if (!Patioventainterfaz.validarDia(anio, mes, dia)) {
-                    Toast.makeText(mainview.getContext(), "Día inválido", Toast.LENGTH_SHORT).show();
-                    dia_ed.setText("");
-                    c++;
-                }
-            }
-
-            EditText cedula_ed = mainview.findViewById(R.id.cedula_pe_vn_etxt);
-            String cedula_str = cedula_ed.getText().toString();
-            if(cedula_str.isEmpty()){
-                Toast.makeText(mainview.getContext(), "Campo vacío: *Cédula*", Toast.LENGTH_SHORT).show();
-                c++;
-            }else{
-                if(cedula_str.length()!=10){
-                    Toast.makeText(mainview.getContext(), "Número de cédula inválido", Toast.LENGTH_SHORT).show();
-                    cedula_ed.setText("");
-                }
-            }
-
-            EditText telefono_ed = mainview.findViewById(R.id.telefono_pe_vn_etxt);
-            String telefono_str = telefono_ed.getText().toString();
-            if(telefono_str.isEmpty()){
-                Toast.makeText(mainview.getContext(), "Campo vacío: *Teléfono*", Toast.LENGTH_SHORT).show();
-                c++;
-            }else{
-                if (telefono_str.length() != 10) {
-                    Toast.makeText(mainview.getContext(), "Numero de telefono invalido", Toast.LENGTH_SHORT).show();
-                    telefono_ed.setText("");
-                    c++;
-                }
-            }
-
-            EditText correo_ed = mainview.findViewById(R.id.correo_pe_vn_etxt);
-            String correo_str = correo_ed.getText().toString();
-            if(correo_str.isEmpty()){
-                Toast.makeText(mainview.getContext(), "Campo vacío: *Correo*", Toast.LENGTH_SHORT).show();
-                c++;
-            }else{
-                if (!Patioventainterfaz.validarMail(correo_str)) {
-                    Toast.makeText(mainview.getContext(), "Correo no valido", Toast.LENGTH_SHORT).show();
-                    correo_ed.setText("");
-                    c++;
-                }
-            }
-
-            EditText textoclave = mainview.findViewById(R.id.contrasenia_pe_vn_etxt);
-            String clave_str = textoclave.getText().toString();
-            EditText textorepetirclave = mainview.findViewById(R.id.clave_confirm_pe_vn_etxt);
-            String repetirclave_str = textorepetirclave.getText().toString();
-            if(!clave_str.isEmpty() && !repetirclave_str.isEmpty()){
-                    if ( (clave_str.compareTo(repetirclave_str) != 0)) {
-                        Toast.makeText(mainview.getContext(), "Las claves no coinciden", Toast.LENGTH_SHORT).show();
-                        textoclave.setText("");
-                        textorepetirclave.setText("");
-                        c++;
-                    }else{
-                        cambiar_clave = true;
-                    }
-            }
-
-            if (c == 0) {
-                String fecha = dia_str + "-" + mes_str + "-" + anio_str;
-                try {
-                    if(cambiar_clave) {
-                        user.cambiarDatosVendedor(
-                                nombre_ed.getText().toString(),
-                                cedula_ed.getText().toString(),
-                                telefono_ed.getText().toString(),
-                                correo_ed.getText().toString(),
-                                fecha,
-                                clave_str);
-                    }else{
-                        user.cambiarDatosVendedor(
-                                nombre_ed.getText().toString(),
-                                cedula_ed.getText().toString(),
-                                telefono_ed.getText().toString(),
-                                correo_ed.getText().toString(),
-                                fecha,
-                                user.getClave());
-                    }
-
-                    if (foto != null) {
-                        StorageReference filePath = mStorageRef.child("Vendedores").child(cedula_str + ".jpg");
-                        filePath.putFile(foto);
-                    }
-
+                if (foto != null) {
+                    StorageReference filePath = mStorageRef.child("Vendedores").child(cedula_str + ".jpg");
+                    filePath.putFile(foto);
                     user.setImagen(String.format("%s.jpg",cedula_ed.getText().toString()));
-
-                    if (patio.buscarVendedores("Cedula", user.getCedula()) != null) {
-                        Toast.makeText(mainview.getContext(), "Se actualizaron los datos correctamente", Toast.LENGTH_SHORT).show();
-                        irPerfil();
-                        verPerfil();
-                    }
-
-                }catch (Exception e) {
-                    Toast.makeText(mainview.getContext(), "No se pudo actualizar la información del vendedor", Toast.LENGTH_SHORT).show();
                 }
+                if (patio.buscarVendedores("Cedula", user.getCedula()) != null) {
+                    Toast.makeText(mainview.getContext(), "Se actualizaron los datos correctamente", Toast.LENGTH_SHORT).show();
+                    irPerfil();
+                    verPerfil();
+                }
+            }catch (Exception e) {
+                Toast.makeText(mainview.getContext(), "Error 180: No se puede mostrar la información", Toast.LENGTH_SHORT).show();
             }
-
+        }else{
+            Toast.makeText(mainview.getContext(), "Existen campos vacíos", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void openGalery() {
@@ -355,7 +333,7 @@ public class Perfil_Vendedor_Fragment extends Fragment {
                 foto = data.getData();
                 ven_img_btn.setImageURI(foto);
             }else{
-                Toast.makeText(mainview.getContext(), "No se ha insertado la imagen", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mainview.getContext(), "Error 181: No se ha insertado la imagen", Toast.LENGTH_SHORT).show();
             }
         }
     }
